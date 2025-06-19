9cc956df1aa4410c64d3462b880053799f0ad245
DenisKolgatin <dkolgatin@yandex.ru>
2025-06-19 17:31:59 +0300

    Дополнил программу

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,7 @@ public class Main {
             System.out.println(products[i] + " " + prices[i] + " рублей.");
         }
         double summ = 0; //цена всей корзины
-        System.out.println("Введите номер товара и его количество через пробел. Введите для завершения" +
-                " команду end, для очистки корзины команду clr");
+        System.out.println("Введите номер товара и его количество через пробел. Введите для завершения" + " команду end, для очистки корзины команду clr");
         while (true) {
             Scanner input = new Scanner(System.in);
             String inputString = input.nextLine();
@@ -31,9 +30,9 @@ public class Main {
                 allProducts[productNumber - 1] = allProducts[productNumber - 1] + productCount;
             }
 
-            System.out.println("Всего товаров в корзине: " );
+            System.out.println("Всего товаров в корзине: ");
             for (int i = 0; i < products.length; i++) {
-                System.out.println(products[i] + " " + allProducts[i] + " штук," + " всего " + prices[i]*allProducts[i] + " рублей.");
+                System.out.println(products[i] + " " + allProducts[i] + " штук," + " всего " + prices[i] * allProducts[i] + " рублей.");
             }
             System.out.println("Сумма корзины " + summ + " рублей.");
         }
