abea829dbf4d1f2eb4cb430291fdca4ffbd8d39a
DenisKolgatin <dkolgatin@yandex.ru>
2025-06-19 17:31:22 +0300

    Дополнил программу

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String[] products = {"Хлеб", "Яблоки 1 кг", "Молоко", "Пиво Балтика 9", "Чипсы 'Русская картошка' 100 грамм"};
+        String[] products = {"Хлеб", "Яблоки", "Молоко", "Пиво Балтика", "Чипсы 'Русская картошка'"};
         int[] prices = {100, 200, 130, 60, 80};
+        int[] allProducts = new int[products.length];
         for (int i = 0; i < products.length; i++) {
             System.out.println(products[i] + " " + prices[i] + " рублей.");
         }
-        double Summ = 0; //цена всей корзины
+        double summ = 0; //цена всей корзины
         System.out.println("Введите номер товара и его количество через пробел. Введите для завершения" +
                 " команду end, для очистки корзины команду clr");
         while (true) {
@@ -16,17 +17,25 @@ public class Main {
             String inputString = input.nextLine();
             if (inputString.equals("end")) {
                 break;
+            } else if (inputString.equals("clr")) {
+                summ = 0;
+                for (int i = 0; i < allProducts.length; i++) {
+                    allProducts[i] = 0;
+                }
+
+            } else {
+                String[] line = inputString.split(" ");
+                int productNumber = Integer.parseInt(line[0]);
+                int productCount = Integer.parseInt(line[1]);
+                summ = summ + productCount * prices[productNumber - 1];
+                allProducts[productNumber - 1] = allProducts[productNumber - 1] + productCount;
             }
-            if (inputString.equals("clr")) {
-                Summ = 0;
-                System.out.println("Сумма корзины " + Summ);
-                continue;
+
+            System.out.println("Всего товаров в корзине: " );
+            for (int i = 0; i < products.length; i++) {
+                System.out.println(products[i] + " " + allProducts[i] + " штук," + " всего " + prices[i]*allProducts[i] + " рублей.");
             }
-            String[] line = inputString.split(" ");
-            int productNumber = Integer.parseInt(line[0]);
-            int productCount = Integer.parseInt(line[1]);
-            Summ = Summ + productCount * prices[productNumber - 1];
-            System.out.println("Сумма корзины " + Summ);
+            System.out.println("Сумма корзины " + summ + " рублей.");
         }
 
     }
